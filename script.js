9f9a023e395b9024968c93d553e3c11ae4221d4f
Dracosomething <larsfalk08@gmail.com>
2024-12-18 13:51:54 +0100

    made movement and started work on attack function

--- script.js
+++ script.js
@@ -21,6 +21,15 @@ function generateField() {
             if (!cell) { return; }
             const row = cell.parentElement;
             console.log(cell.innerHTML, row.rowIndex, cell.cellIndex);
+
+            var selected = document.getElementsByClassName("selected");
+            if (selected.length != 0) {
+                for (let i = 0; i <= selected.length; i++) {
+                    selected[0].classList.remove("selected");
+                    // possibleSquares[0].removeEventListener("click", possibleSquares, true);
+                }
+            }
+
             var possibleSquares = document.getElementsByClassName("possible");
             console.log(possibleSquares.length)
             if (possibleSquares.length != 0) {
@@ -33,46 +42,52 @@ function generateField() {
                 }
             }
 
-            // if (rowList[row.rowIndex - 1] != null) {
-            //     console.log("tedsfwef")
-            //     cellListUp = rowList[row.rowIndex - 1].cells;
-            //     if (cellListUp[cell.cellIndex].classList.contains("black")) {
-            //         console.log("black nono up")
-            //     } else if (cellListUp[cell.cellIndex].classList.contains("white")){
-            //         console.log("yip up")
-            //     }
-            //     if (cellListUp[cell.cellIndex + 1].classList.contains("black")) {
-            //         console.log("black nono up")
-            //     } else if (cellListUp[cell.cellIndex + 1].classList.contains("white")){
-            //         console.log("yip up")
-            //     }
-            //     if (cellListUp[cell.cellIndex - 1].classList.contains("black")) {
-            //         console.log("black nono up")
-            //     } else if (cellListUp[cell.cellIndex - 1].classList.contains("white")){
-            //         console.log("yip up")
-            //     }
-            // }
-            if (rowList[row.rowIndex] != null) {
-                cellList = rowList[row.rowIndex].cells;
-                if (cellList[cell.cellIndex - 1] != null && cellList[cell.cellIndex - 1].classList.contains("black")) {
-                    console.log("black nono")
-                } else if (cellList[cell.cellIndex - 1] != null && cellList[cell.cellIndex - 1].classList.contains("white")) {
-                    console.log("yip here")
-                    if (!cellList[cell.cellIndex - 1].classList.contains("occupied")) {
-                        cellList[cell.cellIndex - 1].classList.add("possible")
-                        cellList[cell.cellIndex - 1].addEventListener("click", move)
-                    }
+            if (rowList[row.rowIndex - 1] != null) {
+                console.log("tedsfwef")
+                cellList = rowList[row.rowIndex - 1].cells;
+                if (cellList[cell.cellIndex] != null && cellList[cell.cellIndex].classList.contains("black")) {
+                    console.log("black nono up")
+                } else if (cellList[cell.cellIndex] != null && cellList[cell.cellIndex].classList.contains("white") && cellList[cell.cellIndex].classList.contains("occupied") && !cellList[cell.cellIndex].children[0].classList.contains("White-Piece")){
+                    console.log("yip up")
                 }
                 if (cellList[cell.cellIndex + 1] != null && cellList[cell.cellIndex + 1].classList.contains("black")) {
-                    console.log("black nono")
-                } else if (cellList[cell.cellIndex + 1] != null && cellList[cell.cellIndex + 1].classList.contains("white")) {
-                    console.log("yip here")
-                    if (!cellList[cell.cellIndex + 1].classList.contains("occupied")) {
-                        cellList[cell.cellIndex + 1].classList.add("possible")
-                        cellList[cell.cellIndex + 1].addEventListener("click", move)
+                    console.log("black nono up")
+                } else if (cellList[cell.cellIndex + 1] != null && cellList[cell.cellIndex + 1].classList.contains("white") && cellList[cell.cellIndex + 1].classList.contains("occupied") && !cellList[cell.cellIndex + 1].children[0].classList.contains("White-Piece")){
+                    console.log("yip up")
+                }
+                if (cellList[cell.cellIndex - 1] != null && cellList[cell.cellIndex - 1].classList.contains("black")) {
+                    console.log("black nono up")
+                } else if (cellList[cell.cellIndex - 1] != null && cellList[cell.cellIndex - 1].classList.contains("white") && cellList[cell.cellIndex - 1].classList.contains("occupied") && !cellList[cell.cellIndex - 1].children[0].classList.contains("White-Piece")){
+                    console.log("yip up")
+                    cellList = rowList[row.rowIndex - 2].cells;
+                    if (!cellList[cell.cellIndex - 2].classList.contains("occupied")) {
+                        console.log("yippppppp")
+                        cellList[cell.cellIndex - 2].classList.add("possible")
+                        cellList[cell.cellIndex - 2].addEventListener("click", attack)
                     }
                 }
             }
+            // if (rowList[row.rowIndex] != null) {
+            //     cellList = rowList[row.rowIndex].cells;
+            //     if (cellList[cell.cellIndex - 1] != null && cellList[cell.cellIndex - 1].classList.contains("black")) {
+            //         console.log("black nono")
+            //     } else if (cellList[cell.cellIndex - 1] != null && cellList[cell.cellIndex - 1].classList.contains("white")) {
+            //         console.log("yip here")
+            //         if (!cellList[cell.cellIndex - 1].classList.contains("occupied")) {
+            //             cellList[cell.cellIndex - 1].classList.add("possible")
+            //             cellList[cell.cellIndex - 1].addEventListener("click", move)
+            //         }
+            //     }
+            //     if (cellList[cell.cellIndex + 1] != null && cellList[cell.cellIndex + 1].classList.contains("black")) {
+            //         console.log("black nono")
+            //     } else if (cellList[cell.cellIndex + 1] != null && cellList[cell.cellIndex + 1].classList.contains("white")) {
+            //         console.log("yip here")
+            //         if (!cellList[cell.cellIndex + 1].classList.contains("occupied")) {
+            //             cellList[cell.cellIndex + 1].classList.add("possible")
+            //             cellList[cell.cellIndex + 1].addEventListener("click", move)
+            //         }
+            //     }
+            // }
             if (rowList[row.rowIndex + 1] != null) {
                 cellList = rowList[row.rowIndex + 1].cells;
                 if (cellList[cell.cellIndex] != null && cellList[cell.cellIndex].classList.contains("black")) {
@@ -103,7 +118,8 @@ function generateField() {
                     }
                 }
             }
-
+            console.log(e.target)
+            e.target.classList.add("selected")
             // console.log(cellList[cell.cellIndex])
             // console.log(cellListUp[cell.cellIndex])
             // console.log(cellListLow[cell.cellIndex])
@@ -122,6 +138,15 @@ function generateField() {
             if (!cell) { return; }
             const row = cell.parentElement;
             console.log(cell.innerHTML, row.rowIndex, cell.cellIndex);
+
+            var selected = document.getElementsByClassName("selected");
+            if (selected.length != 0) {
+                for (let i = 0; i <= selected.length; i++) {
+                    selected[0].classList.remove("selected");
+                    // possibleSquares[0].removeEventListener("click", possibleSquares, true);
+                }
+            }
+
             var possibleSquares = document.getElementsByClassName("possible");
             console.log(possibleSquares.length)
             if (possibleSquares.length != 0) {
@@ -164,45 +189,48 @@ function generateField() {
                     }
                 }
             }
-            if (rowList[row.rowIndex] != null) {
-                cellList = rowList[row.rowIndex].cells;
-                if (cellList[cell.cellIndex - 1] != null && cellList[cell.cellIndex - 1].classList.contains("black")) {
-                    console.log("black nono")
-                } else if (cellList[cell.cellIndex - 1] != null && cellList[cell.cellIndex - 1].classList.contains("white")) {
-                    console.log("yip here")
-                    if (!cellList[cell.cellIndex - 1].classList.contains("occupied")) {
-                        cellList[cell.cellIndex - 1].classList.add("possible")
-                        cellList[cell.cellIndex - 1].addEventListener("click", move)
-                    }
-                }
-                if (cellList[cell.cellIndex + 1] != null && cellList[cell.cellIndex + 1].classList.contains("black")) {
-                    console.log("black nono")
-                } else if (cellList[cell.cellIndex + 1] != null && cellList[cell.cellIndex + 1].classList.contains("white")) {
-                    console.log("yip here")
-                    if (!cellList[cell.cellIndex + 1].classList.contains("occupied")) {
-                        cellList[cell.cellIndex + 1].classList.add("possible")
-                        cellList[cell.cellIndex + 1].addEventListener("click", move)
-                    }
-                }
-            }
-            // if (rowList[row.rowIndex + 1] != null) {
-            //     cellListLow = rowList[row.rowIndex + 1].cells;
-            //     if (cellListLow[cell.cellIndex].classList.contains("black")) {
-            //         console.log("black nono low")
-            //     } else if (cellListLow[cell.cellIndex].classList.contains("white")){
-            //         console.log("yip low")
+            // if (rowList[row.rowIndex] != null) {
+            //     cellList = rowList[row.rowIndex].cells;
+            //     if (cellList[cell.cellIndex - 1] != null && cellList[cell.cellIndex - 1].classList.contains("black")) {
+            //         console.log("black nono")
+            //     } else if (cellList[cell.cellIndex - 1] != null && cellList[cell.cellIndex - 1].classList.contains("white")) {
+            //         console.log("yip here")
+            //         if (!cellList[cell.cellIndex - 1].classList.contains("occupied")) {
+            //             cellList[cell.cellIndex - 1].classList.add("possible")
+            //             cellList[cell.cellIndex - 1].addEventListener("click", move)
+            //         }
             //     }
-            //     if (cellListLow[cell.cellIndex + 1].classList.contains("black")) {
-            //         console.log("black nono low")
-            //     } else if (cellListLow[cell.cellIndex + 1].classList.contains("white")){
-            //         console.log("yip low")
-            //     }
-            //     if (cellListLow[cell.cellIndex - 1].classList.contains("black")) {
-            //         console.log("black nono low")
-            //     } else if (cellListLow[cell.cellIndex - 1].classList.contains("white")){
-            //         console.log("yip low")
+            //     if (cellList[cell.cellIndex + 1] != null && cellList[cell.cellIndex + 1].classList.contains("black")) {
+            //         console.log("black nono")
+            //     } else if (cellList[cell.cellIndex + 1] != null && cellList[cell.cellIndex + 1].classList.contains("white")) {
+            //         console.log("yip here")
+            //         if (!cellList[cell.cellIndex + 1].classList.contains("occupied")) {
+            //             cellList[cell.cellIndex + 1].classList.add("possible")
+            //             cellList[cell.cellIndex + 1].addEventListener("click", move)
+            //         }
             //     }
             // }
+            if (rowList[row.rowIndex - 1] != null) {
+                cellList = rowList[row.rowIndex + 1].cells;
+                if (cellList[cell.cellIndex] != null && cellList[cell.cellIndex].classList.contains("black")) {
+                    console.log("black nono low")
+                } else if (cellList[cell.cellIndex] != null && cellList[cell.cellIndex].classList.contains("white") && !cellList[cell.cellIndex + 1].classList.contains("occupied")){
+                    console.log("yip low")
+                }
+                if (cellList[cell.cellIndex + 1] != null && cellList[cell.cellIndex + 1].classList.contains("black")) {
+                    console.log("black nono low")
+                } else if (cellList[cell.cellIndex + 1] != null && cellList[cell.cellIndex + 1].classList.contains("white") && !cellList[cell.cellIndex].classList.contains("occupied")){
+                    console.log("yip low")
+                }
+                if (cellList[cell.cellIndex - 1] != null && cellList[cell.cellIndex - 1].classList.contains("black")) {
+                    console.log("black nono low")
+                } else if (cellList[cell.cellIndex - 1] != null && cellList[cell.cellIndex - 1].classList.contains("white") && cellList[cell.cellIndex - 1].classList.contains("occupied")){
+                    console.log("yip low")
+                }
+            }
+
+            e.target.classList.add("selected")
+
 
             // console.log(cellList[cell.cellIndex])
             // console.log(cellListUp[cell.cellIndex])
@@ -216,5 +244,23 @@ function generateField() {
 }
 
 function move(event){
-    console.log("code            " + event)
+    console.log("code            " + event + "                   " + document.getElementsByClassName("selected")[0].parentElement)
+    document.getElementsByClassName("selected")[0].parentElement.classList.remove("occupied")
+    event.target.appendChild(document.getElementsByClassName("selected")[0])
+    event.target.classList.add("occupied")
+    var possibleSquares = document.getElementsByClassName("possible");
+    console.log(possibleSquares.length)
+    if (possibleSquares.length != 0) {
+        for (let i = 0; i <= possibleSquares.length; i++) {
+            console.log(i)
+            console.log(possibleSquares[0])
+            possibleSquares[0].removeEventListener("click", move);
+            possibleSquares[0].classList.remove("possible");
+            // possibleSquares[0].removeEventListener("click", possibleSquares, true);
+        }
+    }
+}
+
+function attack(event){
+    console.log("attack")
 }
