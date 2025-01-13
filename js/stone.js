2af45cb09c6905f03cda5b3dc207ad9235f78c57
Dracosomething <larsfalk08@gmail.com>
2025-01-13 12:26:47 +0100

    reworked attacking function

--- js/stone.js
+++ js/stone.js
@@ -88,17 +88,25 @@ class stone {
         player.children[0].innerHTML = piece._turn
     }
 
-    attack(event) {
+    attack(target, newSquare) {
         document.getElementsByClassName("selected")[0].parentElement.classList.remove("occupied")
-        event.target.appendChild(document.getElementsByClassName("selected")[0])
-        event.target.classList.add("occupied")
+        newSquare.appendChild(document.getElementsByClassName("selected")[0])
+        newSquare.classList.add("occupied")
         var possibleSquares = document.getElementsByClassName("possible");
-        document.getElementsByClassName("target")[target].children[0].parentElement.classList.remove("occupied")
-        document.getElementsByClassName("target")[target].children[0].remove()
+        target.parentElement.classList.remove("occupied")
+        target.remove()
         let x = possibleSquares.length;
         if (possibleSquares.length != 0) {
             for (let i = 0; i < x; i++) {
-                possibleSquares[0].removeEventListener("click", piece.attack);
+                if(
+                    possibleSquares[0].removeEventListener("click", piece.BottomLeft) ||
+                    possibleSquares[0].removeEventListener("click", piece.BottomRight) ||
+                    possibleSquares[0].removeEventListener("click", piece.TopLeft) ||
+                    possibleSquares[0].removeEventListener("click", piece.TopRight)
+                ) {
+
+                }
+                // possibleSquares[0].removeEventListener("click", piece.attack);
                 possibleSquares[0].classList.remove("possible");
             }
         }
@@ -120,6 +128,50 @@ class stone {
         player.children[0].innerHTML = piece._turn
     }
 
+    TopLeft(event){
+        let target = event.target
+        console.log(target)
+        const cell = target.closest("td")
+        if (!cell) { return; }
+        const row = cell.parentElement;
+        let newSquare = rowList[row.rowIndex + 1].cells[cell.cellIndex - 1]
+        console.log(newSquare)
+        piece.attack(target, newSquare)
+    }
+
+    TopRight(event){
+        let target = event.target
+        console.log(target)
+        const cell = target.closest("td")
+        if (!cell) { return; }
+        const row = cell.parentElement;
+        let newSquare = rowList[row.rowIndex + 1].cells[cell.cellIndex + 1]
+        console.log(newSquare)
+        piece.attack(target, newSquare)
+    }
+
+    BottomRight(event){
+        let target = event.target
+        console.log(target)
+        const cell = target.closest("td")
+        if (!cell) { return; }
+        const row = cell.parentElement;
+        let newSquare = rowList[row.rowIndex - 1].cells[cell.cellIndex + 1]
+        console.log(newSquare)
+        piece.attack(target, newSquare)
+    }
+
+    BottomLeft(event){
+        let target = event.target
+        console.log(target)
+        const cell = target.closest("td")
+        if (!cell) { return; }
+        const row = cell.parentElement;
+        let newSquare = rowList[row.rowIndex - 1].cells[cell.cellIndex - 1]
+        console.log(newSquare)
+        piece.attack(target, newSquare)
+    }
+
     forceAttack(stone, opposing) {
         const cell = stone.closest('td');
         if (!cell) { return; }
@@ -131,26 +183,10 @@ class stone {
             } else if (cellList[cell.cellIndex - 2] != null && cellList[cell.cellIndex - 1].classList.contains("white") && cellList[cell.cellIndex - 1].classList.contains("occupied") && cellList[cell.cellIndex - 1].children[0].classList.contains(opposing + "-Piece")) {
                 cellList = rowList[row.rowIndex + 2].cells;
                 if (!cellList[cell.cellIndex - 2].classList.contains("occupied")) {
-                    cellList[cell.cellIndex - 2].classList.add("possible")
-                    cellList[cell.cellIndex - 2].addEventListener("click", function () {
-                        switch (document.getElementsByClassName("target").length) {
-                            case 2:
-                                getTarget(1)
-                                break;
-                            case 3:
-                                getTarget(1)
-                                break;
-                            case 4:
-                                getTarget(1);
-                                break;
-                            case 1:
-                                getTarget(0);
-                                break;
-                        }
-                    })
-                    cellList[cell.cellIndex - 2].addEventListener("click", piece.attack)
                     cellList = rowList[row.rowIndex + 1].cells
+                    cellList[cell.cellIndex - 1].addEventListener("click", piece.TopLeft)
                     cellList[cell.cellIndex - 1].classList.add("target")
+                    cellList[cell.cellIndex - 1].classList.add("possible")
                     stone.classList.add("selected")
                     piece.possible = true
                 }
@@ -160,26 +196,10 @@ class stone {
             } else if (cellList[cell.cellIndex + 2] != null && cellList[cell.cellIndex + 1].classList.contains("white") && cellList[cell.cellIndex + 1].classList.contains("occupied") && cellList[cell.cellIndex + 1].children[0].classList.contains(opposing + "-Piece")) {
                 cellList = rowList[row.rowIndex + 2].cells;
                 if (!cellList[cell.cellIndex + 2].classList.contains("occupied")) {
-                    cellList[cell.cellIndex + 2].classList.add("possible")
-                    cellList[cell.cellIndex + 2].addEventListener("click", function () {
-                        switch (document.getElementsByClassName("target").length) {
-                            case 2:
-                                getTarget(0)
-                                break;
-                            case 3:
-                                getTarget(0)
-                                break;
-                            case 4:
-                                getTarget(0);
-                                break;
-                            case 1:
-                                getTarget(0);
-                                break;
-                        }
-                    })
-                    cellList[cell.cellIndex + 2].addEventListener("click", piece.attack)
                     cellList = rowList[row.rowIndex + 1].cells;
+                    cellList[cell.cellIndex + 2].addEventListener("click", piece.TopRight)
                     cellList[cell.cellIndex + 1].classList.add("target")
+                    cellList[cell.cellIndex + 1].classList.add("possible")
                     stone.classList.add("selected")
                     piece.possible = true
                 }
@@ -191,148 +211,29 @@ class stone {
             } else if (cellList[cell.cellIndex - 2] != null && cellList[cell.cellIndex - 1].classList.contains("white") && cellList[cell.cellIndex - 1].classList.contains("occupied") && cellList[cell.cellIndex - 1].children[0].classList.contains(opposing + "-Piece")) {
                 cellList = rowList[row.rowIndex - 2].cells;
                 if (!cellList[cell.cellIndex - 2].classList.contains("occupied")) {
-                    cellList[cell.cellIndex - 2].classList.add("possible")
-                    cellList[cell.cellIndex - 2].addEventListener("click", function () {
-                        switch (document.getElementsByClassName("target").length) {
-                            case 2:
-                                getTarget(1)
-                                break;
-                            case 3:
-                                getTarget(2)
-                                break;
-                            case 4:
-                                getTarget(3);
-                                break;
-                            case 1:
-                                getTarget(0);
-                                break;
-                        }
-                    })
-                    cellList[cell.cellIndex - 2].addEventListener("click", piece.attack)
                     cellList = rowList[row.rowIndex - 1].cells;
+                    cellList[cell.cellIndex - 1].addEventListener("click", piece.BottomLeft)
                     cellList[cell.cellIndex - 1].classList.add("target")
+                    cellList[cell.cellIndex - 1].classList.add("possible")
                     stone.classList.add("selected")
                     piece.possible = true
                 }
             }
             cellList = rowList[row.rowIndex - 1].cells;
             if (cellList[cell.cellIndex + 1] != null && cellList[cell.cellIndex + 1].classList.contains("black")) {
+                console.log("erwrwer")
             } else if (cellList[cell.cellIndex + 2] != null && cellList[cell.cellIndex + 1].classList.contains("white") && cellList[cell.cellIndex + 1].classList.contains("occupied") && cellList[cell.cellIndex + 1].children[0].classList.contains(opposing + "-Piece")) {
-                cellList = rowList[row.rowIndex - 2].cells;
+                cellList = rowList[row.rowIndex + 2].cells;
+                console.log(cellList[cell.cellIndex])
                 if (!cellList[cell.cellIndex + 2].classList.contains("occupied")) {
-                    cellList[cell.cellIndex + 2].classList.add("possible")
-                    cellList[cell.cellIndex + 2].addEventListener("click", function () {
-                        switch (document.getElementsByClassName("target").length) {
-                            case 2:
-                                getTarget(1)
-                                break;
-                            case 3:
-                                getTarget(2)
-                                break;
-                            case 4:
-                                getTarget(2);
-                                break;
-                            case 1:
-                                getTarget(0);
-                                break;
-                        }
-                    })
-                    cellList[cell.cellIndex + 2].addEventListener("click", piece.attack)
-                    cellList = rowList[row.rowIndex - 1].cells;
+                    cellList = rowList[row.rowIndex + 1].cells;
+                    cellList[cell.cellIndex + 2].addEventListener("click", piece.BottomLeft)
                     cellList[cell.cellIndex + 1].classList.add("target")
+                    cellList[cell.cellIndex + 1].classList.add("possible")
                     stone.classList.add("selected")
                     piece.possible = true
                 }
             }
         }
-        if (document.getElementsByClassName("selected") != null) {
-            // console.log(row.rowIndex == (0 || 1 || 10 || 11))
-            if (document.getElementsByClassName("selected").length > 1) {
-                if (rowList[row.rowIndex + 2] != null && rowList[row.rowIndex - 2] != null) {
-                    if (!rowList[row.rowIndex + 2].cells[cell.cellIndex].classList.contains("possible")) {
-                        if (!document.getElementsByClassName("selected").length < 1) {
-                            console.log("terewrw")
-                            let selected = document.getElementsByClassName("selected")
-                            for (let i = 0; i < document.getElementsByClassName("selected").length; i++) {
-                                document.getElementsByClassName("selected")[0].classList.remove("selected")
-                            }
-                            for (let x = 0; x < document.getElementsByClassName("possible").length; x++) {
-                                document.getElementsByClassName("possible")[x].removeEventListener("click", piece.attack)
-                                if (x != 0) {
-                                    document.getElementsByClassName("possible")[x].classList.remove("possible")
-                                }
-                            }
-                            document.getElementsByClassName("possible")[0].addEventListener("click", piece.attack)
-                            selected[0].classList.add("selected")
-                            piece.possible = true
-                        }
-                        piece.possible = true
-                    } else if (!rowList[row.rowIndex - 2].cells[cell.cellIndex].classList.contains("possible")) {
-                        let selected = document.getElementsByClassName("selected")
-                        for (let i = 0; i < document.getElementsByClassName("selected").length; i++) {
-                            document.getElementsByClassName("selected")[0].classList.remove("selected")
-                        }
-                        for (let x = 0; x < document.getElementsByClassName("possible").length; x++) {
-                            document.getElementsByClassName("possible")[x].removeEventListener("click", piece.attack)
-                            if (x != 0) {
-                                document.getElementsByClassName("possible")[x].classList.remove("possible")
-                            }
-                        }
-                        document.getElementsByClassName("possible")[0].addEventListener("click", piece.attack)
-                        selected[0].classList.add("selected")
-                        piece.possible = true
-                    }
-                } else if (row.rowIndex == 0 || row.rowIndex == 1 || row.rowIndex == 10 || row.rowIndex == 11) {
-                    console.log(rowList[row.rowIndex + 2] != null)
-                    console.log(rowList[row.rowIndex - 2] != null)
-                    if (rowList[row.rowIndex + 2] != null && !rowList[row.rowIndex + 2].cells[cell.cellIndex].classList.contains("possible")) {
-                        console.log("terewrw")
-                        let selected = document.getElementsByClassName("selected")
-                        document.getElementsByClassName("selected")[1].classList.remove("selected")
-                        for (let x = 0; x < document.getElementsByClassName("possible").length; x++) {
-                            document.getElementsByClassName("possible")[x].removeEventListener("click", piece.attack)
-                            if (x != 1) {
-                                document.getElementsByClassName("possible")[x].classList.remove("possible")
-                            }
-                        }
-                        document.getElementsByClassName("possible")[0].addEventListener("click", piece.attack)
-                        selected[1].classList.add("selected")
-                        piece.possible = true
-                        piece.possible = true
-                    } else if (rowList[row.rowIndex - 2] != null && !rowList[row.rowIndex - 2].cells[cell.cellIndex].classList.contains("possible")) {
-                        console.log("efwef")
-                        let selected = document.getElementsByClassName("selected")
-                        document.getElementsByClassName("selected")[1].classList.remove("selected")
-                        for (let x = 0; x < document.getElementsByClassName("possible").length; x++) {
-                            document.getElementsByClassName("possible")[x].removeEventListener("click", piece.attack)
-                            if (x != 1) {
-                                document.getElementsByClassName("possible")[x].classList.remove("possible")
-                            }
-                        }
-                        document.getElementsByClassName("possible")[0].addEventListener("click", piece.attack)
-                        selected[0].classList.add("selected")
-                        piece.possible = true
-                    }
-                }
-            } else if (document.getElementsByClassName("selected").length == 1) {
-                let numb = Math.floor(Math.random() * document.getElementsByClassName("selected").length)
-                if (document.getElementsByClassName("selected").length > 1) {
-                    for (let i = 0; i < document.getElementsByClassName("selected").length; i++) {
-                        document.getElementsByClassName("selected")[0].classList.remove("selected")
-                    }
-                    for (let x = 0; x < document.getElementsByClassName("possible").length; x++) {
-                        document.getElementsByClassName("possible")[0].removeEventListener("click", piece.attack)
-                        if (x != numb) {
-                            document.getElementsByClassName("possible")[x].classList.remove("possible")
-                        }
-                    }
-                }
-                console.log(document.getElementsByClassName("possible"))
-                document.getElementsByClassName("possible")[0].addEventListener("click", piece.attack)
-                document.getElementsByClassName("selected")[numb].classList.add("selected")
-            }
-        }
-        piece.possible = true
-
     }
 }
